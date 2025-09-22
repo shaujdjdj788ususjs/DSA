6d2c1bf8ead80e272de807af29bd534b271eb043
shamita kulkarni <viratshamita@gmail.com>
2025-09-22 23:38:03 +0530

    add

---
+++ Solution.java
@@ -0,0 +1,60 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Solution {
+
+    // Function to add two numbers represented by linked lists
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+            int sum = carry;
+
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+        }
+
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        // Input linked list 1: 2 -> 4 -> 3
+        ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+
+        // Input linked list 2: 5 -> 6 -> 4
+        ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+
+        Solution sol = new Solution();
+        ListNode result = sol.addTwoNumbers(l1, l2);
+
+        // Print the result linked list
+        while (result != null) {
+            System.out.print(result.val + " ");
+            result = result.next;
+        }
+    }
+}
